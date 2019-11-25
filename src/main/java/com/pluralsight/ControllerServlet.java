e7383c68c92a51a2749a5abee546c88c8084486b
sun4csit <sunil6ptr@gmail.com>
2019-11-25 08:13:32 +0530

    Completed module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -11,63 +11,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.inject.Inject;
+
 /**
  * Servlet implementation class HelloWorld
  */
 
 public class ControllerServlet extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-		private DBConnection dbConnection;
+	private static final long serialVersionUID = 1L;
+	private DBConnection dbConnection;
 
-		@Inject
-    private BookDAO bookDAO;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-
-    public void init() {
-			dbConnection = new DBConnection();
-			bookDAO = new BookDAO(dbConnection.getConnection());
-    }
-
-		public void destroy() {
-			dbConnection.disconnect();
-		}
-
-    public ControllerServlet() {
-        super();
-    }
+	@Inject
+	private BookDAO bookDAO;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	public void init() {
+		dbConnection = new DBConnection();
+		bookDAO = new BookDAO(dbConnection.getConnection());
+	}
+
+	public void destroy() {
+		dbConnection.disconnect();
+	}
+
+	public ControllerServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException  {
+			throws ServletException, IOException {
 		String action = request.getPathInfo();
-
+		System.out.println("Action URL from PathInfo : " + action);
 		try {
-			switch(action) {
-				case "/admin":
-					 showBookAdmin(request, response);
-           break;
-			  case "/new":
-					showNewForm(request, response);
-          break;
-				case "/insert":
-					insertBook(request, response);
-          break;
-			 	case "/edit":
-				 	showEditForm(request, response);
-          break;
-				case "/delete":
-					deleteBook(request, response);
-          break;
-				case "/update":
-					updateBook(request, response);
-          break;
-        default:
-				   listBooks(request, response);
-           break;
+			switch (action) {
+			case "/admin":
+				showBookAdmin(request, response);
+				break;
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertBook(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/delete":
+				deleteBook(request, response);
+				break;
+			case "/update":
+				updateBook(request, response);
+				break;
+			default:
+				listBooks(request, response);
+				break;
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -101,18 +104,18 @@ public class ControllerServlet extends HttpServlet {
 
 	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-				int id = Integer.parseInt(request.getParameter("id"));
-		    Book existingBook = bookDAO.getBook(id);
-		    RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
-		    request.setAttribute("book", existingBook);
-		    dispatcher.forward(request, response);
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book existingBook = bookDAO.getBook(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", existingBook);
+		dispatcher.forward(request, response);
 	}
 
 	private void deleteBook(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-				int id = Integer.parseInt(request.getParameter("id"));
-				bookDAO.deleteBook(id);
-				response.sendRedirect("list");
+		int id = Integer.parseInt(request.getParameter("id"));
+		bookDAO.deleteBook(id);
+		response.sendRedirect("list");
 	}
 
 	private void insertBook(HttpServletRequest request, HttpServletResponse response)
@@ -142,9 +145,11 @@ public class ControllerServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.println("This is the doPost() method!");
